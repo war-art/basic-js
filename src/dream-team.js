65abd8b3b5ff93755aa19d63d95a31d6402bffd4
war-art <70770404+war-art@users.noreply.github.com>
2020-10-28 12:29:40 +0500

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(Array.isArray(members)) {
+		return members.map(e => {
+      if(typeof(e) == "string") 
+        return e.trim().toUpperCase()[0];}).sort().join("");
+	} else {
+		return false;
+	}
 };
