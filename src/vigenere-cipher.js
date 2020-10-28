2cf36e1d34b0548b79ec0f06f0d3c3c199fa5b0c
war-art <70770404+war-art@users.noreply.github.com>
2020-10-28 12:44:16 +0500

    Update vigenere-cipher.js

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
+    this.alpha = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+  }
+
+  encrypt(msg, key) {
+    const message = [...msg.toUpperCase()];
+    const keys = [...key.toUpperCase()];
+    let pos = 0;
+    let result = message.map( el => {
+      if (this.alpha.includes(el)) {
+        let idx = (this.alpha.indexOf(keys[pos]) + this.alpha.indexOf(el)) % this.alpha.length;
+        pos = ++pos % keys.length;
+        return this.alpha[idx];
+      }
+      return el;
+    } );
+    return this.mode ? result.join('') : result.reverse().join('');
+  }
+
+  decrypt(msg, key) {
+    const message = [...msg.toUpperCase()];
+    const keys = [...key.toUpperCase()];
+    let pos = 0;
+    let result = message.map( el => {
+      if (this.alpha.includes(el)) {
+        let idx = this.alpha.indexOf(el) - this.alpha.indexOf(keys[pos]);
+        if (idx < 0) idx += this.alpha.length;
+        pos = ++pos % keys.length;
+        return this.alpha[idx];
+      }
+      return el;
+    } );
+    return this.mode ? result.join('') : result.reverse().join('');
   }
 }
 
