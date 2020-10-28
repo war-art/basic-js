4a5fd05883eb9c742c4022d02f088ab055f20ffc
war-art <70770404+war-art@users.noreply.github.com>
2020-10-28 12:31:19 +0500

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let result = [];
+	
+	if(options.separator == undefined) options.separator = '+';
+  if(options.addition === undefined) options.addition = '';
+  if(options.addition == null) options.addition = 'null';
+  if(options.repeatTimes == undefined) options.repeatTimes = 1;
+  if(options.additionSeparator == undefined) options.additionSeparator == '';
+  if(options.additionRepeatTimes == undefined) options.additionRepeatTimes = 1;
+	
+	for(let i = 0; i < options.repeatTimes; i++) {
+		let prStr = [];
+		for(let j = 0; j < options.additionRepeatTimes; j++) {
+			prStr.push(options.addition);
+		}
+		result.push(str + prStr.join(options.additionSeparator));
+	}
+	
+  return result.join(options.separator);
 };
-  
